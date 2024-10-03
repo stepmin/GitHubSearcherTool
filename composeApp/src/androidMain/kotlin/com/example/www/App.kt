923303548a6140fc7022d0814e02f4f68db9311a
Štěpán Minařík <rohliq@gmail.com>
2024-10-03 22:25:04 +0200

    added Top Bar, added Colors definition

--- composeApp/src/androidMain/kotlin/com/example/www/App.kt
+++ composeApp/src/androidMain/kotlin/com/example/www/App.kt
@@ -1,23 +1,40 @@
 package com.example.www
 
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.www.ui.theme.backgroundsPrimaryLight
 import kotlinx.serialization.Serializable
 
 @Serializable
 object ListDestination
 
 @Composable
-fun App() {
+fun App(
+    navController: NavHostController = rememberNavController(),
+) {
+    //TODO-dark theme
     MaterialTheme(
-        colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()
+        colorScheme = lightColorScheme()
     ) {
         Surface {
             val navController = rememberNavController()
@@ -30,5 +47,50 @@ fun App() {
                 }
             }
         }
+
+        Scaffold(
+            topBar = {
+                TopBar(
+                    actionBarTitle = stringResource(R.string.app_name),
+                )
+            }
+        ) { paddingValues ->
+
+            NavHost(
+                navController = navController,
+                startDestination = ListDestination,
+                modifier = Modifier.padding(paddingValues)
+            ) {
+                composable<ListDestination> {
+                    //TODO
+                }
+            }
+        }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    actionBarTitle: String,
+) {
+    val searchBarState = remember { mutableStateOf(false) }
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(searchBarState.value) {
+        if (searchBarState.value) {
+            focusRequester.requestFocus()
+        }
+    }
+
+    TopAppBar(
+        title = {
+            Text(
+                text = actionBarTitle,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(containerColor = backgroundsPrimaryLight),
+    )
 }
---
+++ composeApp/src/androidMain/kotlin/com/example/www/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+// Colors.kt
+package com.example.www.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Colors
+val accentPrimaryLight = Color(0xFF0000FF)
+val backgroundsPrimaryLight = Color(0xFFF4F4F9)
+val backgroundsSecondaryLight = Color(0xFFC8102E)
+val backgroundsTertiaryLight = Color(0xFFFFFFFF)
+val foregroundsPrimaryLight = Color(0xFF000000)
+val foregroundsSecondaryLight = Color(0xFF666666)
+val foregroundsTertiaryLight = Color(0xFFB3B3B3)
+val foregroundsSeparatorLight = Color(0xFFF0F0F0)
+val foregroundsOnPrimaryLight = Color(0xFFFFFFFF)
+val iconsPrimaryLight = Color(0xFF000000)
+val iconsSecondaryLight = Color(0xFFB3B3B3)
+val iconsTertiaryLight = Color(0xFF0000FF)
