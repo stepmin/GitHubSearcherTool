5bf0abf310823481ea5dd95dc7ea872052245f2c
Štěpán Minařík <rohliq@gmail.com>
2024-10-03 21:56:09 +0200

    added logging to Ktor

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -40,6 +40,7 @@ kotlin {
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
             implementation(libs.koin.core)
+            implementation(libs.ktor.client.logging)
             api(libs.kmp.observable.viewmodel)
         }
 
--- shared/src/commonMain/kotlin/com/example/www/di/Koin.kt
+++ shared/src/commonMain/kotlin/com/example/www/di/Koin.kt
@@ -2,6 +2,10 @@ package com.example.www.di
 
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.http.ContentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
@@ -13,6 +17,10 @@ val dataModule = module {
     single {
         val json = Json { ignoreUnknownKeys = true }
         HttpClient {
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.ALL
+            }
             install(ContentNegotiation) {
                 // TODO Fix API so it serves application/json
                 json(json, contentType = ContentType.Any)
