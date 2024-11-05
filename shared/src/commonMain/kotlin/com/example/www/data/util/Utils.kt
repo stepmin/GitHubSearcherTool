f1690935993e5d42981489ea2d252b9612c3f057
Štěpán Minařík <rohliq@gmail.com>
2024-11-05 22:01:18 +0100

    Cosmetics

--- shared/src/commonMain/kotlin/com/example/www/data/util/Utils.kt
+++ shared/src/commonMain/kotlin/com/example/www/data/util/Utils.kt
@@ -3,6 +3,5 @@ package com.example.www.data.util
 sealed interface Result<out R> {
     class Success<out R>(val value: R) : Result<R>
     data object Loading : Result<Nothing>
-
     class Error(val throwable: Throwable) : Result<Nothing>
 }
