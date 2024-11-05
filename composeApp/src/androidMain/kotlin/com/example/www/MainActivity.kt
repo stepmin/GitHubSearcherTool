613ca705947cacc371eb87c4ae59682d536fa877
Štěpán Minařík <rohliq@gmail.com>
2024-11-05 22:03:02 +0100

    refactoring

--- composeApp/src/androidMain/kotlin/com/example/www/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/www/MainActivity.kt
@@ -4,15 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.www.ui.ComposeApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ComposeApp {
-                finish()
-            }
+            ComposeApp()
         }
     }
 }
